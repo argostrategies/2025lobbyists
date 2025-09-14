2bd8d45d40dde20dfb0991a702884fcca5206393
argostrategies <jason@argo.us>
2025-09-13 21:59:43 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useMemo } from "react";
-import { Input } from "@/components/ui/input";
-import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "./components/ui/input";
+import { Card, CardContent } from "./components/ui/card";
 
 /**
  * === IMPORTANT ===
